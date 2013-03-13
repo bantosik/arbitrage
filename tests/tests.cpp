e6391ce5dd4d9aed20ca5aa4260cfdf5a0aa205b
Bartek Antosik <antosik.bartek@gmail.com>
2013-03-13 20:54:06 +0100

    Compiling version of project

---
+++ tests/tests.cpp
@@ -0,0 +1,12 @@
+#define BOOST_TEST_MAIN
+#if !defined( WIN32 )
+    #define BOOST_TEST_DYN_LINK
+#endif
+#include <boost/test/unit_test.hpp>
+#include "arbitrage_controller.h"
+
+BOOST_AUTO_TEST_CASE( arbitrage_controller_should )
+{
+	    BOOST_CHECK_EQUAL(function(), 0);
+	    BOOST_FAIL("Now we are talking");
+}
